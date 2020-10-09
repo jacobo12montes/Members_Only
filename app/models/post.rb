f8df6d7691fef06de010e9ddf2ead6dcd8ba0209
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-10-09 01:58:49 +0100

    Validate Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :member
+  validates :title, presence: true, length: { minimum: 2 }, allow_blank: false
+  validates :body, presence: true, length: { maximum: 1000 }, allow_blank: false
 end

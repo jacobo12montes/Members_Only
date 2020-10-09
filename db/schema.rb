7dc0c97b75c7df05f9d4f80c90580b245f97d751
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-10-09 01:55:39 +0100

    Make Post Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_002255) do
+ActiveRecord::Schema.define(version: 2020_10_09_005426) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_10_09_002255) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
   end
 
 end

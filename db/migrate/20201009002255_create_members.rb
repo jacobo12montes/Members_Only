c80d0be243ccbd11fba2acf44ea2eedddc9ee50a
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-10-09 01:25:06 +0100

    create Member model

---
+++ db/migrate/20201009002255_create_members.rb
@@ -0,0 +1,11 @@
+class CreateMembers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :members do |t|
+      t.string :name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end

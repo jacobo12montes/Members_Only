c6bcda8df3410e2fbb06058cedb25c9918ff7025
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-10-09 01:32:57 +0100

    Validate Member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+    has_many :posts
+    validates :name, presence: true, length: { minimum: 2 }, allow_blank: false, uniqueness: true
+    validates :email, confirmation: true, presence: true, uniqueness: true, allow_blank: false
+    validates :password, presence: true, length: { minimum: 10 }, allow_blank: false
 end

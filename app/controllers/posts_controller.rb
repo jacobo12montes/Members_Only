cc221efadb94611284ffcec277000215b1c71860
Juan <jacobo12.montes@gmail.com>
2020-10-15 15:01:31 -0500

    Add the logic to show the author only to the login members

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,24 @@
 class PostsController < ApplicationController
   
-  before_action :require_login, only: [:new, :create]
+   before_action :authenticate_member!, only: [:new, :create]
   
-  
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params)
+
+    @post = current_member.posts.build(post_params)
 
     if @post.save
       redirect_to posts_path
     else
       render :new
     end
-  end
-
-  def index
-    @posts = Post.all
   end
 
   def show
